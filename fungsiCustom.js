c32401c5ffea865295c8f2dbb7384f7e3f762f7a
Mohamad Saefudin <thismohamads@gmail.com>
2022-06-12 01:43:08 +0700

    feat : Challenge JS Callback Part 2 152235865101650 - Mohamad Saefudin

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf-8",
+    // Fungsi Callback
+    (err, valSatu) => {
+      const dataSatu = JSON.parse(valSatu);
+      const messageSatu = kataKedua(dataSatu.message);
+
+      fs.readFile(file2, "utf-8",
+        // Fungsi Callback
+        (err, valDua) => {
+          const dataDua = JSON.parse(valDua);
+          const messageDua = kataKedua(dataDua[0].message);
+
+          fs.readFile(file3, "utf-8",
+            // Fungsi Callback
+            (err, valTiga) => {
+              const dataTiga = JSON.parse(valTiga);
+              const messageTiga = kataKedua(dataTiga[0].data.message);
+
+              fnCallback(err, [messageSatu, messageDua, messageTiga]);
+            });
+        });
+    });
+};
+
+const kataKedua = (kata) => {
+  const kataAkhir = kata.split(" ").reverse();
+
+  return kataAkhir[0];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
